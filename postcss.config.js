d4d45ea3203ef7e26f317a084de5d249e5e3e29e
eseoooo <esegboria@gmail.com>
2024-01-27 00:58:39 -0600

    add more configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    autoprefixer: {},
     tailwindcss: {},
     autoprefixer: {},
-  },
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
 };
