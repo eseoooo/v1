d5f24ad1174d76b4d4adc737848ea6bd86950fb7
eseoooo <esegboria@gmail.com>
2024-01-31 00:03:59 -0600

    change  navlink styling

--- components/header/nav-bar.js
+++ components/header/nav-bar.js
@@ -3,12 +3,12 @@ import NavLink from "@/components/header/nav-link";
 
 export default function NavBar() {
   return (
-    <nav className="h-16 lg:h-17.5 w-lvw border-b border-dark-800 text-sm px-12">
-      <ul className="hidden md:flex h-full justify-end items-center gap-x-2">
+    <nav className="h-16 lg:h-17.5 w-lvw border-b border-dark-800 text-sm px-12 backdrop-blur backdrop-grayscale">
+      <ol className="hidden md:flex h-full justify-end items-center gap-x-2">
         {navLinks.map((link) => (
           <NavLink key={link.name} name={link.name} href={link.href} />
         ))}
-      </ul>
+      </ol>
     </nav>
   );
 }
--- components/header/nav-link.js
+++ components/header/nav-link.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function NavLink({ name, href }) {
   return (
-    <li className="h-full tracking-widest">
+    <li className="h-full">
       <Link
-        className="p-3 inline-flex h-full items-center border-b-2 border-b-transparent hover:border-b-2 hover:border-b-gold-700 hover:text-gold-700"
+        className="p-3 inline-flex tracking-widest uppercase font-medium h-full items-center border-b-2 border-r-2 border-transparent  hover:border-gold-700 hover:text-gold-700 "
         href={href}
       >
         {name}
