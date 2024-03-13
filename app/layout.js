cfc3113f415074fad64eb83f36a326c587e75570
eseoooo <esegboria@gmail.com>
2024-03-13 07:52:50 -0500

    add logo and favicons

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,37 @@ import NotificationProvider from "@/lib/context/notification-context";
 
 export const metadata = {
   title: "Esegboria Osarhemen",
-  description:
-    "Esegboria Osarhemen is a software engineer who builds accessible, inclusive products and digital experiences for the web.",
+  description: `Esegboria Osarhemen is a software engineer who builds accessible, 
+    inclusive products and digital experiences for the web.`,
+  icons: {
+    apple: {
+      url: "/images/favicon/apple-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+    icon: [
+      {
+        url: "/images/favicon/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        url: "/images/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/images/favicon/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/images/favicon/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
---
+++ components/header/home-link.js
@@ -0,0 +1,14 @@
+"use client";
+import Logo from "@/public/icons/logo.svg";
+
+export default function HomeLink() {
+  return (
+    <a
+      href="/"
+      aria-label="home"
+      className="focused-link !outline-offset-4 w-12 lg:w-14 text-gold-700"
+    >
+      <Logo aria-hidden="true" />
+    </a>
+  );
+}
