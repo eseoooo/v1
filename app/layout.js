bb2cb392a25a2723fca11d51e511be476ee1ce71
eseoooo <esegboria@gmail.com>
2024-03-29 22:59:03 -0500

    add more metadata

--- app/layout.js
+++ app/layout.js
@@ -4,40 +4,12 @@ import Header from "@/components/header/header";
 import SkipToContent from "@/components/ui/skip-to-content";
 import NotificationProvider from "@/lib/context/notification-context";
 import FeatureProvider from "@/lib/context/feature-context";
+import { metadataObject } from "@/lib/const";
 
-export const metadata = {
-  title: "Esegboria Osarhemen",
-  description: `Esegboria Osarhemen is a software engineer who builds accessible, 
-    inclusive products and digital experiences for the web.`,
-  icons: {
-    apple: {
-      url: "/images/favicon/apple-icon.png",
-      sizes: "180x180",
-      type: "image/png",
-    },
-    icon: [
-      {
-        url: "/images/favicon/favicon-16x16.png",
-        sizes: "16x16",
-        type: "image/png",
-      },
-      {
-        url: "/images/favicon/favicon-32x32.png",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        url: "/images/favicon/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        url: "/images/favicon/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
-  },
+export const metadata = { ...metadataObject };
+
+export const viewport = {
+  themeColor: "#151418",
 };
 
 export default function RootLayout({ children }) {
